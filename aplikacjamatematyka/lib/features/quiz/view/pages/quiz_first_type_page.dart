d8a6ee6236bc23502ef32f03547944465a0aa78f
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-11-18 10:45:57 +0100

    fix: stateful widgt in first type quiz

--- aplikacjamatematyka/lib/features/quiz/view/pages/quiz_first_type_page.dart
+++ aplikacjamatematyka/lib/features/quiz/view/pages/quiz_first_type_page.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+class QuizFirstTypePage extends StatefulWidget {
+  const QuizFirstTypePage({super.key});
+
+  @override
+  State<QuizFirstTypePage> createState() => _QuizFirstTypePageState();
+}
+
+class _QuizFirstTypePageState extends State<QuizFirstTypePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

f8f8ae6a23e7813b45694d11b4900c9130c5200a
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-11-23 12:14:03 +0100

    fix: removed appbar in lesson content page

--- aplikacjamatematyka/lib/features/quiz/view/pages/lesson_content_page.dart
+++ aplikacjamatematyka/lib/features/quiz/view/pages/lesson_content_page.dart
@@ -1,5 +1,3 @@
-
-import 'package:aplikacjamatematyka/features/quiz/view/widgets/appbar_widget.dart';
 import 'package:aplikacjamatematyka/features/quiz/view/widgets/content_lesson_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikacjamatematyka/features/quiz/viewmodel/lesson_content_page.dart';
@@ -18,7 +16,6 @@ class _LessonContentPageState extends State<LessonContentPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 165, 12, 192),
-      appBar: const AppbarWidget(),
       body: Padding(
         padding: const EdgeInsets.only(top: 20.0),
         child: Container(

03d1f0edd8e5af905bb8ead866582eeadbc47e08
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-11-25 19:47:50 +0100

    feat: map with questions and answers

--- aplikacjamatematyka/lib/features/quiz/view/pages/quiz_first_type_page.dart
+++ aplikacjamatematyka/lib/features/quiz/view/pages/quiz_first_type_page.dart
@@ -1,3 +1,4 @@
+import 'package:aplikacjamatematyka/features/quiz/data/questions.dart';
 import 'package:aplikacjamatematyka/features/quiz/view/widgets/answer_button_first_type.dart';
 import 'package:flutter/material.dart';
 class QuizFirstTypePage extends StatefulWidget {
@@ -10,11 +11,21 @@ class QuizFirstTypePage extends StatefulWidget {
 class _QuizFirstTypePageState extends State<QuizFirstTypePage> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return Column(
+     mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("Pytanie: "),
+        Text(currentQuestion.text),
         const SizedBox(height: 30),
-        AnswerButtonFirstType(text: 'Odpowiedz1', onTap: () {})
+        //musi być to list bo mapa potrzebuje, a 3 kropki to spread operator. Rozyspuje liste widgetow na zewnatrz do innej listy
+         ...currentQuestion.answers.map((answer) {
+        return AnswerButtonFirstType(
+        text: answer,
+        onTap: () {
+          
+        },
+      );
+    }).toList(),
     ],
     );
   }

f0182f8863fbfb6c404c373ca0693499d9a1204d
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-12-09 23:43:04 +0100

    fix: removing not needed files

--- aplikacjamatematyka/lib/features/quiz/viewmodel/lesson_content_page.dart
+++ aplikacjamatematyka/lib/features/quiz/viewmodel/lesson_content_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:aplikacjamatematyka/core/data/notifiers.dart';
-import 'package:aplikacjamatematyka/features/quiz/view/pages/test_test_page.dart';
 
 class LessonContentPageViewmodel {
   void onButtonPressed() {
@@ -8,12 +7,6 @@ class LessonContentPageViewmodel {
   }
   void onBackButtonPressed(){
     selectedPageNotifier.value = 0;
-  }
-    void onTestButtonPressed(context){
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => TestTestPage()),
-    );
   }
    void onLessonButtonPressed(){
     selectedPageNotifier.value = 5;

22d1eff10c4d9f0640ce71a1f5b15e38496597b4
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-12-11 08:29:56 +0100

    feat: model of the quiz third type

---
+++ aplikacjamatematyka/lib/features/quiz/model/QuizQuestionThirdType.dart
@@ -0,0 +1,11 @@
+class QuizQuestionThirdType {
+  final List<String> left;
+  final List<String> right;
+  final Map<String, String> correct;
+
+  QuizQuestionThirdType({
+    required this.left,
+    required this.right,
+    required this.correct,
+  });
+}

2510e5e8d969fa88dcf53e80f03c9dc226ebe9a8
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-12-11 08:33:15 +0100

    feat: temporary questions for third type

---
+++ aplikacjamatematyka/lib/features/quiz/data/questionsThird.dart
@@ -0,0 +1,18 @@
+import 'package:aplikacjamatematyka/features/quiz/model/QuizQuestionThirdType.dart';
+
+final questionsThird = [
+  QuizQuestionThirdType(
+  left: ['2 + 3', '4 × 2', '15 − 6', '9 ÷ 3'],
+  right: ['5', '8', '9', '3'],
+  correct: {
+    '2 + 3': '5',
+    '4 × 2': '8',
+    '15 − 6': '9',
+    '9 ÷ 3': '3',
+  },
+)
+  Tempquizquestionfirsttype(
+    'Jaką najmniejszą liczbą naturalną jest:',
+    ['tak'],
+  ),
+];

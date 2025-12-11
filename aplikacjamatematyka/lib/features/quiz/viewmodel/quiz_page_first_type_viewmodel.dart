c51c30c545613e8fd97f972fb85426949b9daede
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-12-11 08:37:30 +0100

    fix: some changes and new files

--- aplikacjamatematyka/lib/features/quiz/viewmodel/quiz_page_first_type_viewmodel.dart
+++ aplikacjamatematyka/lib/features/quiz/viewmodel/quiz_page_first_type_viewmodel.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import '../data/questions.dart';
-//import '../data/questionsSecond.dart';
 
-class QuizPageViewModel extends ChangeNotifier {
+class QuizPageFirstTypeViewModel extends ChangeNotifier {
   int currentQuestionIndex = 0;
   String? selectedAnswer;
   List<String> shuffledAnswers = [];
   final TextEditingController answerController = TextEditingController();
 
 
-  QuizPageViewModel() {
+  QuizPageFirstTypeViewModel() {
     _shuffleCurrentQuestionAnswers();
   }
 

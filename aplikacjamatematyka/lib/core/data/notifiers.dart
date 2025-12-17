d05fa2c1b8827f6aefa0967419fcdab4eb8cb876
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-12-17 22:25:43 +0100

    feat: gradient in finish quiz with white square

--- aplikacjamatematyka/lib/core/data/notifiers.dart
+++ aplikacjamatematyka/lib/core/data/notifiers.dart
@@ -1,6 +1,6 @@
 //ValueNotifier: hold the data
 //ValueListaenableBuilder: listentothedata (don't need the setstate)
 import 'package:flutter/material.dart';
-ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
+ValueNotifier<int> selectedPageNotifier = ValueNotifier(12);
 ValueNotifier<bool> isDarkModifier = ValueNotifier(false);
 ValueNotifier<String> tempLessonName = ValueNotifier("");
--- aplikacjamatematyka/lib/features/quiz/view/pages/finish_quiz_page.dart
+++ aplikacjamatematyka/lib/features/quiz/view/pages/finish_quiz_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:aplikacjamatematyka/features/quiz/viewmodel/finish_quiz_page_viewmodel.dart';
-import 'package:aplikacjamatematyka/features/quiz/view/widgets/appbars/appbar_lesson_widget.dart';
 import 'package:aplikacjamatematyka/core/theme/app_pallete.dart';
 
 class FinishQuizPage extends StatefulWidget {
@@ -15,43 +14,46 @@ class _FinishQuizPageState extends State<FinishQuizPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 165, 12, 192),
-
-      appBar: AppbarLessonWidget(
-        onBack: widget.viewModel.onBackButtonPressed,
-      ),
-
-      body: Column(
-        children: [
-          Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 30),
-                  padding: const EdgeInsets.all(22),
-                  decoration: BoxDecoration(
-                    color: Pallete.whiteColor,
-                    borderRadius: BorderRadius.circular(24),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Pallete.blackColor.withOpacity(0.1),
-                        blurRadius: 20,
-                        offset: const Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: 
-                  Container(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        
-                      }, 
-                      child: 
-                      Text("Gratulacje Quizu"),
-            
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+              Pallete.purpleColor,
+              Pallete.purplemidColor,
+              Pallete.whiteColor,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )
+        ),
+        child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 520,
+                maxHeight: 720,
+                minWidth: 300,
+                minHeight: 400,
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  color: Pallete.whiteColor,
+                  borderRadius: BorderRadius.circular(24),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Pallete.blackColor.withOpacity(0.1),
+                      blurRadius: 20,
+                      offset: const Offset(0, 4),
                     ),
-                  
-                  ),
+                  ],
                 ),
-        ],
-
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Gratulacje Quizu"),
+                ),
+              ),
+            ),
+          ),
       ),
     );
   }
